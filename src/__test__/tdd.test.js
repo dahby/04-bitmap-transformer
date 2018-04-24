4afcf68d13b7f86740dd936b2fbfd025965578d8
Panos <panosrockbear@gmail.com>
2018-04-23 21:56:31 -0700

    test created, not verified

--- src/__test__/tdd.test.js
+++ src/__test__/tdd.test.js
@@ -5,8 +5,27 @@ import { fstat } from "fs";
 const fs = require('fs');
 
 describe('Testing dim.js',()=>{
-fs.readfile(`${__dirname}/__test__/asset/${process.argv[3]}.bmp`);
-
+    let resultFile;
+    let initialFile;
+    fs.readFile( // Vinicio - readFile is an async operation
+        `${__dirname}/__test__/asset/${process.argv[3]}.bmp`,
+        (error, fileBuffer) => { // [?]
+          if (error) {
+            throw error;
+          }
+          initialFile=fileBuffer;
+        },
+      );
+    fs.readFile( // Vinicio - readFile is an async operation
+        `${__dirname}/__test__/asset/${process.argv[2]}.bmp`,
+        (error, fileBuffer) => { // [?]
+          if (error) {
+            throw error;
+          }
+          resultFile=fileBuffer;
+        },
+      );
+      expect(resultFile[151]).toEqual(initialFile[151]/2);
 });
 
 describe('Testing intensify.js',()=>{
